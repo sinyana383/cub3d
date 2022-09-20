bdf0baa7d09668e958b51de0ce60f5961247400a
ddurrand <ddurrand@student.21-school.ru>
2022-09-20 19:14:00 +0300

    draw_floor_and_celling added

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ddurrand <ddurrand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 14:16:12 by ddurrand          #+#    #+#             */
-/*   Updated: 2022/09/19 15:31:02 by ddurrand         ###   ########.fr       */
+/*   Updated: 2022/09/20 19:11:55 by ddurrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,33 @@
 # define CUB3D_H
 # include <stdlib.h>
 
-typedef struct s_cub3d
-{
-	t_map	*map;
-}	t_cub3d;
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 typedef struct s_map
 {
-	char			**map;			// карта
+	int				**map;			// карта, из char в int можно просто (int)var
 	char			*path[4];		// пути к текстуркам: [0,1,2,3] - north south east west
 	unsigned int	celling_color;	// 0,1,2,3 байты - прозрач,red,green,blue
+	unsigned int	floor_color;
 }	t_map;
 
+typedef struct s_cub3d
+{
+	void	*mlx;
+	t_map	*map;
+}	t_cub3d;
+
+
+// DESINES
+# define WIN_WIDTH	640
+# define WIN_HEIGHT	480
+# define FOV 		60		// угол обзора(в градусах)
 
 #endif
 
@@ -43,5 +58,9 @@ typedef struct s_map
 					2. angle -> collision points
 					3. collision points -> high of line
 					4. print line by pixels
+				- или реализовать перпендикулярным ветором к направлению
 		- hook
+	- Bonus
+		- миникарта - это отрисовка so_long-a
+		только в маленьком размере в одном из углу
 */
--- src/main.c
+++ src/main.c
@@ -6,13 +6,57 @@
 /*   By: ddurrand <ddurrand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 14:12:00 by ddurrand          #+#    #+#             */
-/*   Updated: 2022/09/19 14:21:56 by ddurrand         ###   ########.fr       */
+/*   Updated: 2022/09/20 19:12:08 by ddurrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include "mlx.h"
 
-int	main(int argc, char **argv)
+#include <mlx.h>
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	exit (0);
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	draw_floor_and_celling(t_data *data, int color_celling, int color_floor)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < WIN_HEIGHT / 2)
+	{
+		x = -1;
+		while (++x < WIN_WIDTH)
+			my_mlx_pixel_put(data, x, y, color_celling);
+		++y;
+	}
+	while (y < WIN_HEIGHT)
+	{
+		x = -1;
+		while (++x < WIN_WIDTH)
+			my_mlx_pixel_put(data, x, y, color_floor);
+		++y;
+	}
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d");
+	img.img = mlx_new_image(mlx, WIN_WIDTH, WIN_HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
+								&img.line_length, &img.endian);
+	draw_floor_and_celling(&img, 0x00F5F7F0, 0x00A284AB);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
